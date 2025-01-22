baa29a1615f1dd6c9e30a664835240666b435ee3
Serg Mukhin <serg.muhin1989@gmail.com>
2025-01-23 00:38:03 +0300

    3.13. На связи база данных

--- project/apps/user-service/src/user/user.entity.ts
+++ project/apps/user-service/src/user/user.entity.ts
@@ -70,7 +70,8 @@ const UserSchema = SchemaFactory.createForClass(User);
 
 UserSchema.pre('save', async function (next) {
   if (this.isModified('password') || this.isNew) {
-    await this.hashPassword();
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
   }
   next();
 });
